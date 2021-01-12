945f838f2cfd35515d0734699e1ceb86f53cf7e3
Micu <andrei.micuda@gmail.com>
2021-01-12 22:10:47 +0200

    Finished distribution gallery implementation & added normal distribution

--- server.R
+++ server.R
@@ -1,5 +1,6 @@
 library(shiny)
 library(hash)
+library(xtable)
 
 outputDir <- "distribution_data"
 
@@ -23,48 +24,80 @@ loadData <- function() {
   data
 }
 
-data_distributions <- hash(
-  "Normal" = hash(
-    "desc_HTML" = "The Normal Distribution is lorem ipsum!",
-    "variables" = data.frame(
-      "name" = c("&sigma;", "&mu;", "a", "b"),
-      "input_id" = c("std_dev", "mean", "a", "b"),
-      "default_value" = c(0, 1, -5, 5)
-    )),
-  "Uniform" = hash(
-    "desc_HTML" = "The Uniform Distribution is lorem ipsum",
-    "variables" = data.frame(
-      "name" = c("a", "b"),
-      "input_id" = c("a", "b"),
-      "default_value" = c(0, 1)
-    ))
-)
-
-
+{
+  data_distributions <- hash(
+    "Normal" = hash(
+      "desc_HTML" = '
+    <div class="panel panel-body">
+      <p>
+        In probability theory, a <b>normal</b> (or <b>Gaussian</b> or <b>Gauss</b> or <b>Laplace–Gauss</b>) <b>distribution</b> is a type of <span class="text-success">continuous probability distribution</span> for a real-valued random variable. The general form of its probability density function is
+      </p>
+      <img src="https://wikimedia.org/api/rest_v1/media/math/render/svg/00cb9b2c9b866378626bcfa45c86a6de2f2b2e40" class="center-block" aria-hidden="true" style="vertical-align: -2.838ex; width:24.446ex; height:6.676ex;" />
+      <br />
+      <p>
+        The parameter &mu; is the <span class="text-success">mean</span> or <span class="text-success">expectation</span> of the distribution (and also its <b>median</b> and <b>mode</b>), while the parameter &sigma; is its <span class="text-success">standard deviation</span>. The <span class="text-success">variance</span> of the distribution is &sigma;<sup>2</sup>.
+      </p>
+      
+      <p>
+        Normal distributions are important in statistics and are often used in the natural and social sciences to <b>represent real-valued random variables whose distributions are not known</b>. Their importance is partly due to the <span class="text-success">central limit theorem</span>. It states that, under some conditions, the average of many samples (observations) of a random variable with finite mean and variance is itself a random variable—whose distribution converges to a normal distribution as the number of samples increases. 
+      </p>
+    </div>
+    
+    ',
+      "variables" = data.frame(
+        "name" = c("&mu;", "&sigma;", "a", "b"),
+        "input_id" = c("mean", "std_dev", "a", "b"),
+        "default_value" = c(0, 1, -5, 5)
+      )),
+    "Uniform" = hash(
+      "desc_HTML" = "The Uniform Distribution is lorem ipsum",
+      "variables" = data.frame(
+        "name" = c("a", "b"),
+        "input_id" = c("a", "b"),
+        "default_value" = c(0, 1)
+      ))
+  )
+}
 
 function(input, output) {
-  distribution_plot_data <- eventReactive(c(input$update_plot, input$dens_mass_n_points), {
+  distribution_plot_data <- eventReactive(c(input$update_plot, input$n_points), {
     distribution_name = paste(input$distribution)
     
     # if the data didn't load yet (update button hasn't been clicked)
     # fill in the data for N(0,1)
     if(length(distribution_name) == 0){
       distribution_name <- 'Normal'
-      points <- seq(-5, 5, length.out = 25)
+      points <- seq(-5, 5, length.out = 20)
       
       hash(
         "desc" = data_distributions[[distribution_name]]$desc_HTML,
-        "plot_values" = dnorm(points, 0, 1),
-        "points" = points)
+        "dens_mass_plot_values" = dnorm(points, 0, 1),
+        "distribution_plot_values" = pnorm(points, 0, 1),
+        "points" = points,
+        "statistics" = hash(
+          "Mean" = 0,
+          "Median" = 0,
+          "Variance" = 1,
+          "Standard deviation" = 1
+        ))
     }
     else {
       if(distribution_name == 'Normal') {
-        points <- seq(input$var_a, input$var_b, length.out = input$dens_mass_n_points)
+        points <- seq(input$var_a, input$var_b, length.out = input$n_points)
+        std_dev <- input$var_std_dev
+        mean <- input$var_mean
         
         hash(
           "desc" = data_distributions[[distribution_name]]$desc_HTML,
-          "plot_values" = dnorm(points, input$var_std_dev, input$var_mean),
-          "points" = points)
+          "dens_mass_plot_values" = dnorm(points, mean, std_dev),
+          "distribution_plot_values" = pnorm(points, mean, std_dev),
+          "points" = points,
+          "statistics" = hash(
+            "Mean" = mean,
+            "Median" = mean,
+            "Variance" = std_dev,
+            "Standard deviation" = sqrt(std_dev)
+          ))
       }
       else if(distribution_name == 'Uniform') {
         
@@ -103,6 +136,27 @@ function(input, output) {
   
   output$density_mass_plot <- renderPlot({
     data <- distribution_plot_data()
-    plot(data$points, data$plot_values)
+    plot(data$points, data$dens_mass_plot_values)
+  })
+  
+  output$distribution_plot <- renderPlot({
+    data <- distribution_plot_data()
+    plot(data$points, data$distribution_plot_values)
+  })
+  
+  output$statistics_table <- renderTable({
+    data <- distribution_plot_data()
+    
+    # we have to convert our 'statistics' hash to a data frame so that it can be rendered to the UI
+    table <- data.frame(matrix(ncol = 2, nrow = 0))
+    col_names <- c("Stat", "Value")
+    colnames(table) <- col_names
+    
+    for(key in keys(data$statistics))
+    {
+      table[nrow(table) + 1,] <- list(key, data$statistics[[key]])
+    }
+    
+    table
   })
 }
--- ui.R
+++ ui.R
@@ -1,16 +1,17 @@
 library(shiny)
-library(shinythemes)
 
 fluidPage(
-  theme = shinytheme("flatly"),
+  theme = "flatly.css",
   navbarPage(
     title = "Shiny RV Project",
     tabPanel("Distributions",
      titlePanel("Distributions of various variables"),
      sidebarLayout(
        sidebarPanel(
+         style = "max-height: 70vh; overflow: scroll;",
          uiOutput("choose_distribution"),
          uiOutput("choose_values"),
+         sliderInput(inputId = "n_points", min = 1, max = 100, value = 20, label = "No. points"),
          actionButton(
            inputId = "update_plot",
            label = "Update"),
@@ -19,25 +20,19 @@ fluidPage(
        mainPanel(
          tabsetPanel(
            tabPanel("Description",
-                    textOutput(outputId = "desc")),
-           tabPanel("Density/Mass Function",
-             fluidRow(
+                    uiOutput(outputId = "desc")),
+           tabPanel("Density/Mass Function", {
                plotOutput(
                 outputId = "density_mass_plot"
-             )),
-             fluidRow(
-               column(6,
-                sliderInput(inputId = "dens_mass_n_points", min = 1, max = 100, value = 20, label = "No. points"),
-                offset = 3)
-             )),
+                )
+             }),
            tabPanel("Distribution Function", {
              plotOutput(
                outputId = "distribution_plot"
              )
            }),
-           tabPanel("Debug", {
-             textOutput(outputId = "debug")
-           })
+           tabPanel("Various Statistics",
+            tableOutput(outputId = "statistics_table"))
          )
        )
      )
