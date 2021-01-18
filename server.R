a738062256353d5ce4dc5671ad9d870407c23112
Micu <andrei.micuda@gmail.com>
2021-01-18 22:26:41 +0200

    Added Poisson Distribution

--- server.R
+++ server.R
@@ -27,56 +27,38 @@ loadData <- function() {
 
 {
   data_distributions <- hash(
-    "Normal" = hash(
-      "desc_HTML" = '
-    <div class="panel panel-body">
-      <p>
-        In probability theory, a <b>normal</b> (or <b>Gaussian</b> or <b>Gauss</b> or <b>Laplace–Gauss</b>) <b>distribution</b> is a type of <span class="text-success">continuous probability distribution</span> for a real-valued random variable. The general form of its probability density function is
-      </p>
-      <img src="https://wikimedia.org/api/rest_v1/media/math/render/svg/00cb9b2c9b866378626bcfa45c86a6de2f2b2e40" class="center-block" aria-hidden="true" style="vertical-align: -2.838ex; width:24.446ex; height:6.676ex;" />
-      <br />
-      <p>
-        The parameter &mu; is the <span class="text-success">mean</span> or <span class="text-success">expectation</span> of the distribution (and also its <b>median</b> and <b>mode</b>), while the parameter &sigma; is its <span class="text-success">standard deviation</span>. The <span class="text-success">variance</span> of the distribution is &sigma;<sup>2</sup>.
-      </p>
-      
-      <p>
-        Normal distributions are important in statistics and are often used in the natural and social sciences to <b>represent real-valued random variables whose distributions are not known</b>. Their importance is partly due to the <span class="text-success">central limit theorem</span>. It states that, under some conditions, the average of many samples (observations) of a random variable with finite mean and variance is itself a random variable—whose distribution converges to a normal distribution as the number of samples increases. 
-      </p>
-    </div>
-    
-    ',
-      "variables" = data.frame(
-        "name" = c("&mu;", "&sigma;", "a", "b"),
-        "input_id" = c("mean", "std_dev", "a", "b"),
-        "default_value" = c(0, 1, -5, 5)
-      )),
-    "Uniform" = hash(
-      "desc_HTML" = '
-      <div class="panel panel-body">
-        <p>
-          A <span class="text-success">uniform</span> distribution, also called a <span class="text-success">rectangular distribution</span>, is a probability distribution that has <span class="text-success">constant probability</span>.
-        </p>
-        
-        <p>
-          This distribution is defined by two parameters, a and b:
-        </p>
-        
-        <ul>
-          <li>a is the minimum.</li>
-          <li>b is the maximum.</li>
-        </ul>
-  
-        <p>
-          The distribution is written as <code>U(a,b)</code>.
+    "Bernoulli" = {hash(
+      "desc_HTML" = '<div class="panel panel-body">
+        <p>A <b>Bernoulli distribution</b> is a <span class="text-success">discrete probability distribution</span> for a Bernoulli trial - a random experiment that has only two outcomes (usually called a "Success" or a "Failure"). For example, the probability of getting a heads (a "success") while flipping a coin is 0.5. The probability of "failure" is 1 - P (1 minus the probability of success, which also equals 0.5 for a coin toss). It is a special case of the <span class="text-success">binomial distribution</span> for n = 1. In other words, it is a binomial distribution with a single trial (e.g. a single coin toss).
         </p>
       </div>
       ',
       "variables" = data.frame(
-        "name" = c("a", "b"),
-        "input_id" = c("a", "b"),
-        "default_value" = c(0, 1)
-      )),
-    "Geometric" = hash(
+        "name" = c("p","x"),
+        "input_id" = c("p","x"),
+        "default_value" = c(0.5,1),
+        "min" = c(0,0),
+        "max" = c(1,1),
+        "step" = c(0.05,1)
+      )
+    )},
+    "Binomial" = {hash(
+      "desc_HTML" = '<div class="panel panel-body">
+       <p>In probability theory and statistics, the <b>binomial distribution</b> with parameters n and p is the <span class="text-success">discrete probability distribution</span> of the number of successes in a sequence of n <b>independent experiments</b>, each asking a yes-no question, and each with its own Boolean-valued outcome: <span class="text-success">success (with probability p) or failure (with probability q = 1 ??? p)</span>. The <b>binomial distribution</b> is a <b>Bernoulli distribution</b>. The binomial distribution is the basis for the popular binomial test of statistical significance.
+
+The <b>binomial distribution</b> is frequently used to model <span class="text-success">the number of successes</span> in a sample of size n drawn with replacement from a population of size N. If the sampling is carried out without replacement, the draws are not independent and so the resulting distribution is a hypergeometric distribution, not a binomial one. However, for N much larger than n, the binomial distribution remains a good approximation, and is widely used. </p>
+      </div>'
+      ,
+      "variables" = data.frame(
+        "name" = c("p","n"),
+        "input_id" = c("p","n"),
+        "default_value" = c(0.5,1),
+        "min" = c(0,1),
+        "max" = c(1,20),
+        "step" = c(0.05,1)
+      )
+    )},
+    "Geometric" = {hash(
       "desc_HTML" = '<div class="panel panel-body">
       <p>The geometric distribution represents the number of failures before you get a success in a series of 
       <a href="https://www.statisticshowto.com/bernoulli-distribution/#trial">Bernoulli trials</a>. 
@@ -101,38 +83,72 @@ loadData <- function() {
         "max" = c(1, 99, 100),
         "step" = c(0.05, 1, 1)
       )
-    ),
-    "Bernoulli" = hash(
-      "desc_HTML" = '<div class="panel panel-body">
-        <p>A <b>Bernoulli distribution</b> is a <span class="text-success">discrete probability distribution</span> for a Bernoulli trial - a random experiment that has only two outcomes (usually called a "Success" or a "Failure"). For example, the probability of getting a heads (a "success") while flipping a coin is 0.5. The probability of "failure" is 1 - P (1 minus the probability of success, which also equals 0.5 for a coin toss). It is a special case of the <span class="text-success">binomial distribution</span> for n = 1. In other words, it is a binomial distribution with a single trial (e.g. a single coin toss).
+    )},
+    "Normal" = {hash(
+      "desc_HTML" = '
+    <div class="panel panel-body">
+      <p>
+        In probability theory, a <b>normal</b> (or <b>Gaussian</b> or <b>Gauss</b> or <b>Laplace–Gauss</b>) <b>distribution</b> is a type of <span class="text-success">continuous probability distribution</span> for a real-valued random variable. The general form of its probability density function is
+      </p>
+      <img src="https://wikimedia.org/api/rest_v1/media/math/render/svg/00cb9b2c9b866378626bcfa45c86a6de2f2b2e40" class="center-block" aria-hidden="true" style="vertical-align: -2.838ex; width:24.446ex; height:6.676ex;" />
+      <br />
+      <p>
+        The parameter &mu; is the <span class="text-success">mean</span> or <span class="text-success">expectation</span> of the distribution (and also its <b>median</b> and <b>mode</b>), while the parameter &sigma; is its <span class="text-success">standard deviation</span>. The <span class="text-success">variance</span> of the distribution is &sigma;<sup>2</sup>.
+      </p>
+      
+      <p>
+        Normal distributions are important in statistics and are often used in the natural and social sciences to <b>represent real-valued random variables whose distributions are not known</b>. Their importance is partly due to the <span class="text-success">central limit theorem</span>. It states that, under some conditions, the average of many samples (observations) of a random variable with finite mean and variance is itself a random variable—whose distribution converges to a normal distribution as the number of samples increases. 
+      </p>
+    </div>
+    
+    ',
+      "variables" = data.frame(
+        "name" = c("&mu;", "&sigma;", "a", "b"),
+        "input_id" = c("mean", "std_dev", "a", "b"),
+        "default_value" = c(0, 1, -5, 5)
+      ))},
+    "Uniform" = {hash(
+      "desc_HTML" = '
+      <div class="panel panel-body">
+        <p>
+          A <span class="text-success">uniform</span> distribution, also called a <span class="text-success">rectangular distribution</span>, is a probability distribution that has <span class="text-success">constant probability</span>.
+        </p>
+        
+        <p>
+          This distribution is defined by two parameters, a and b:
+        </p>
+        
+        <ul>
+          <li>a is the minimum.</li>
+          <li>b is the maximum.</li>
+        </ul>
+  
+        <p>
+          The distribution is written as <img src="https://wikimedia.org/api/rest_v1/media/math/render/svg/7db59725558a835be6944d93345f46a987a46b1e" class="mwe-math-fallback-image-inline" aria-hidden="true" style="vertical-align: -0.838ex; margin-left: -0.038ex; width:6.706ex; height:2.843ex;">.
         </p>
       </div>
       ',
       "variables" = data.frame(
-        "name" = c("p","x"),
-        "input_id" = c("p","x"),
-        "default_value" = c(0.5,1),
-        "min" = c(0,0),
-        "max" = c(1,1),
-        "step" = c(0.05,1)
-      )
-    ),
-    "Binomial" = hash(
-      "desc_HTML" = '<div class="panel panel-body">
-       <p>In probability theory and statistics, the <b>binomial distribution</b> with parameters n and p is the <span class="text-success">discrete probability distribution</span> of the number of successes in a sequence of n <b>independent experiments</b>, each asking a yes-no question, and each with its own Boolean-valued outcome: <span class="text-success">success (with probability p) or failure (with probability q = 1 ??? p)</span>. The <b>binomial distribution</b> is a <b>Bernoulli distribution</b>. The binomial distribution is the basis for the popular binomial test of statistical significance.
-
-The <b>binomial distribution</b> is frequently used to model <span class="text-success">the number of successes</span> in a sample of size n drawn with replacement from a population of size N. If the sampling is carried out without replacement, the draws are not independent and so the resulting distribution is a hypergeometric distribution, not a binomial one. However, for N much larger than n, the binomial distribution remains a good approximation, and is widely used. </p>
-      </div>'
-      ,
+        "name" = c("a", "b"),
+        "input_id" = c("a", "b"),
+        "default_value" = c(0, 1)
+      ))},
+    "Poisson" = {hash(
+      "desc_HTML" = '
+      <div class="panel panel-body">
+        <p>
+          A <span class="text-success">Poisson distribution</span> is a tool that helps to predict the probability of certain events from happening when you know how often the event has occurred. It gives us the <span class="text-success">probability of a given number of events happening in a fixed interval of time</span>.
+        </p>
+      </div>',
       "variables" = data.frame(
-        "name" = c("p","n"),
-        "input_id" = c("p","n"),
-        "default_value" = c(0.5,1),
-        "min" = c(0,1),
-        "max" = c(1,20),
-        "step" = c(0.05,1)
+        "name" = c("&lambda;", "a", "b"),
+        "input_id" = c("lambda", "a", "b"),
+        "default_value" = c(1, 0, 20),
+        "min" = c(1, 0, 0),
+        "max" = c(100, 100, 100),
+        "step" = c(1, 1, 1)
       )
-    )
+    )}
   )
 }
 
@@ -147,6 +163,7 @@ function(input, output) {
       points <- seq(-5, 5, length.out = 20)
       
       hash(
+        "name" = distribution_name,
         "desc" = data_distributions[[distribution_name]]$desc_HTML,
         "dens_mass_plot_values" = dnorm(points, 0, 1),
         "distribution_plot_values" = pnorm(points, 0, 1),
@@ -249,6 +266,7 @@ function(input, output) {
           median <- "[0,1]"
         }
         hash(
+          "name" = distribution_name,
           "desc" = data_distributions[[distribution_name]]$desc_HTML,
           "dens_mass_plot_values" = dbern(points,prob = p),
           "distribution_plot_values" = pbern(points,prob = p),
@@ -271,6 +289,7 @@ function(input, output) {
         mean <- n* p
         median <- n *p
         hash(
+          "name" = distribution_name,
           "desc" = data_distributions[[distribution_name]]$desc_HTML,
           "dens_mass_plot_values" = dbinom( points,size=n,prob=p),
           "distribution_plot_values" = pbinom( points,size=n,prob=p),
@@ -280,6 +299,23 @@ function(input, output) {
             "Median" = median,
             "Variance" = var,
             "Standard deviation" = sqrt(var)
+          ))
+      }
+      else if(distribution_name == 'Poisson') {
+        points <- seq(input$var_a, input$var_b, by=1)
+        lambda <- input$var_lambda
+        
+        hash(
+          "name" = distribution_name,
+          "desc" = data_distributions[[distribution_name]]$desc_HTML,
+          "dens_mass_plot_values" = dpois(points, lambda),
+          "distribution_plot_values" = ppois(points, lambda),
+          "points" = points,
+          "statistics" = hash(
+            "Mean" = lambda,
+            "Median" = floor(lambda + 1/3 - 0.02 / lambda),
+            "Variance" = lambda,
+            "Standard deviation" = sqrt(lambda)
           ))
       }
     } 
@@ -323,9 +359,9 @@ function(input, output) {
   
   output$density_mass_plot <- renderPlot({
     data <- distribution_plot_data()
+    plot_color <- '#428bca'
     if(data$name == 'Uniform')
     {
-      plot_color <- '#428bca'
       a <- data$interval_points$begin
       b <- data$interval_points$end
       # we center the interval and make it take 70% of the whole x axis
@@ -388,14 +424,27 @@ function(input, output) {
         col = '#555555'
       )
     }
+    else if(data$name == 'Poisson')
+    {
+      plot(
+        data$points, data$dens_mass_plot_values,
+        type = 'o',
+        pch = 19,
+        col=plot_color
+        )
+    }
     else
     {
-      plot(data$points, data$dens_mass_plot_values)
+      plot(
+        data$points, data$dens_mass_plot_values,
+        col=plot_color
+        )
     }
   })
   
   output$distribution_plot <- renderPlot({
     data <- distribution_plot_data()
+    plot_color <- '#428bca'
     if(data$name == 'Uniform')
     {
       plot_color <- '#428bca'
@@ -436,9 +485,28 @@ function(input, output) {
         col = plot_color
       )
     }
+    else if(data$name == 'Poisson')
+    {
+      plot(
+        data$points, data$distribution_plot_values,
+        pch = 19,
+        col = plot_color
+      )
+      
+      segments(
+        x0 = data$points,
+        y0 = data$distribution_plot_values,
+        x1 = data$points + 1,
+        y1 = data$distribution_plot_values,
+        col = plot_color
+      )
+    }
     else
     {
-      plot(data$points, data$distribution_plot_values)
+      plot(
+        data$points, data$distribution_plot_values,
+        col=plot_color
+        )
     }
   })
   
